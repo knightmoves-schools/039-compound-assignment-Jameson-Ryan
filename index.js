4355f53d2ae5ddd69b53d24d5d874ef0471cbf66
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-10-30 14:20:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let addition = 50;
+addition += 5;
+let subtraction = 40;
+subtration -= 1;
+let multiplication = 5;
+multiplication *= 7;
+let division = 6;
+divison /= 2;
 
+document.getElementById("result").innerHTML = multiplication;
